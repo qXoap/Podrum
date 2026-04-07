97e0ff1782da0b9d5980a70cd32109d9f93f7312
Vapesito <jbinact@gmail.com>
2026-04-07 02:29:00 -0700

    integrate backend and sessions into loader

--- src/podrum/loader/PodrumLoader.php
+++ src/podrum/loader/PodrumLoader.php
@@ -7,27 +7,50 @@ use pocketmine\utils\SingletonTrait;
 use podrum\loader\factory\ModuleFactory;
 use podrum\moderation\ModerationModule;
 use podrum\protection\ProtectionModule;
+use podrum\loader\factory\Backend;
+use podrum\loader\factory\SessionManager;
+use podrum\loader\task\DatabaseTask;
 
 class PodrumLoader extends PluginBase
 {
-    use SingletonTrait {
-        setInstance as private;
-        reset as private;
-    }
+    use SingletonTrait;
+
+    private Backend $backend;
+    private SessionManager $sessions;
 
     protected function onLoad(): void
     {
         self::setInstance($this);
+        $this->saveDefaultConfig();
     }
 
     protected function onEnable(): void
     {
+        $this->backend = new Backend($this);
+        $this->sessions = new SessionManager($this);
+
+        $this->getServer()->getPluginManager()->registerEvents($this->sessions, $this);
+
+        $this->getScheduler()->scheduleRepeatingTask(new DatabaseTask($this), 20 * 60 * 5);
+
         ModuleFactory::register(new ModerationModule($this));
         ModuleFactory::register(new ProtectionModule($this));
     }
 
+    public function getBackend(): Backend
+    {
+        return $this->backend;
+    }
+
+    public function getSessions(): SessionManager
+    {
+        return $this->sessions;
+    }
+
     protected function onDisable(): void
     {
-
+        if (isset($this->backend)) {
+            $this->backend->shutdown();
+        }
     }
 }
